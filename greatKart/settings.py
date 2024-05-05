4790ad3560af258a49ba6afe401ad410d4167e2c
xcheche <chekwubechukwu23@gmail.com>
2024-05-05 20:44:43 +0100

    static/media configurations added

--- greatKart/settings.py
+++ greatKart/settings.py
@@ -118,12 +118,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+#statcic configuration
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = [
      "greatKart/static",
 ]
+#media configuration
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- greatKart/urls.py
+++ greatKart/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -22,4 +24,4 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.home, name='home')
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
